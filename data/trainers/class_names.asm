d3a9d45da76b28a24aa163b5c88dddd414e67fa4
Trung Lê <8@tle.id.au>
2026-01-31 02:58:06 +1100

    Translate trainer class names to Vietnamese

--- data/trainers/class_names.asm
+++ data/trainers/class_names.asm
@@ -1,71 +1,71 @@
 TrainerClassNames::
 ; entries correspond to trainer classes (see constants/trainer_constants.asm)
 	list_start TRAINER_CLASS_NAME_LENGTH - 1
-	li "LEADER"
-	li "LEADER"
-	li "LEADER"
-	li "LEADER"
-	li "LEADER"
-	li "LEADER"
-	li "LEADER"
-	li "LEADER"
-	li "RIVAL"
-	li "#MON PROF."
-	li "ELITE FOUR"
-	li "<PKMN> TRAINER"
-	li "ELITE FOUR"
-	li "ELITE FOUR"
-	li "ELITE FOUR"
-	li "CHAMPION"
-	li "LEADER"
-	li "LEADER"
-	li "LEADER"
-	li "SCIENTIST"
-	li "LEADER"
-	li "YOUNGSTER"
-	li "SCHOOLBOY"
-	li "BIRD KEEPER"
-	li "LASS"
-	li "LEADER"
-	li "COOLTRAINER"
-	li "COOLTRAINER"
-	li "BEAUTY"
-	li "#MANIAC"
-	li "ROCKET"
-	li "GENTLEMAN"
-	li "SKIER"
-	li "TEACHER"
-	li "LEADER"
-	li "BUG CATCHER"
-	li "FISHER"
-	li "SWIMMER♂"
-	li "SWIMMER♀"
-	li "SAILOR"
-	li "SUPER NERD"
-	li "RIVAL"
-	li "GUITARIST"
-	li "HIKER"
-	li "BIKER"
-	li "LEADER"
-	li "BURGLAR"
-	li "FIREBREATHER"
-	li "JUGGLER"
-	li "BLACKBELT"
-	li "ROCKET"
-	li "PSYCHIC"
-	li "PICNICKER"
-	li "CAMPER"
-	li "ROCKET"
-	li "SAGE"
-	li "MEDIUM"
-	li "BOARDER"
-	li "#FAN"
-	li "KIMONO GIRL"
-	li "TWINS"
-	li "#FAN"
-	li "<PKMN> TRAINER"
-	li "LEADER"
-	li "OFFICER"
-	li "ROCKET"
-	li "MYSTICALMAN"
+	li "THỦ LĨNH"      ; LEADER (Gym Leader)
+	li "THỦ LĨNH"      ; LEADER
+	li "THỦ LĨNH"      ; LEADER
+	li "THỦ LĨNH"      ; LEADER
+	li "THỦ LĨNH"      ; LEADER
+	li "THỦ LĨNH"      ; LEADER
+	li "THỦ LĨNH"      ; LEADER
+	li "THỦ LĨNH"      ; LEADER
+	li "ĐỐI THỦ"      ; RIVAL
+	li "GS.#MON"       ; #MON PROF. (Professor)
+	li "TỨ ĐẠI"        ; ELITE FOUR
+	li "TRAINER"       ; <PKMN> TRAINER
+	li "TỨ ĐẠI"        ; ELITE FOUR
+	li "TỨ ĐẠI"        ; ELITE FOUR
+	li "TỨ ĐẠI"        ; ELITE FOUR
+	li "VÔ ĐỊCH"       ; CHAMPION
+	li "THỦ LĨNH"      ; LEADER
+	li "THỦ LĨNH"      ; LEADER
+	li "THỦ LĨNH"      ; LEADER
+	li "NHÀ KHOA HỌC"  ; SCIENTIST
+	li "THỦ LĨNH"      ; LEADER
+	li "CẬU BÉ"        ; YOUNGSTER
+	li "HỌC SINH"      ; SCHOOLBOY
+	li "CHIM THỦ"      ; BIRD KEEPER
+	li "THIẾU NỮ"      ; LASS
+	li "THỦ LĨNH"      ; LEADER
+	li "CAO THỦ"       ; COOLTRAINER (male)
+	li "CAO THỦ"       ; COOLTRAINER (female)
+	li "MỸ NHÂN"       ; BEAUTY
+	li "FAN #MON"      ; #MANIAC
+	li "ROCKET"        ; ROCKET
+	li "QUÝ ÔNG"       ; GENTLEMAN
+	li "VĐV TRƯỢT"     ; SKIER
+	li "GIÁO VIÊN"     ; TEACHER
+	li "THỦ LĨNH"      ; LEADER
+	li "BẮT SÂU"       ; BUG CATCHER
+	li "CÂU CÁ"        ; FISHER
+	li "BƠI LỘI♂"      ; SWIMMER♂
+	li "BƠI LỘI♀"      ; SWIMMER♀
+	li "THỦY THỦ"      ; SAILOR
+	li "MỌT SÁCH"      ; SUPER NERD
+	li "ĐỐI THỦ"      ; RIVAL
+	li "TAY ĐÀN"       ; GUITARIST
+	li "LEO NÚI"       ; HIKER
+	li "TAY ĐUA"       ; BIKER
+	li "THỦ LĨNH"      ; LEADER
+	li "ĐẠO CHÍCH"     ; BURGLAR
+	li "PHUN LỬA"      ; FIREBREATHER
+	li "TẠP KỸ"        ; JUGGLER
+	li "VÕ SĨ"         ; BLACKBELT
+	li "ROCKET"        ; ROCKET
+	li "THẦN GIAO"     ; PSYCHIC
+	li "DÃ NGOẠI"      ; PICNICKER
+	li "CẮM TRẠI"      ; CAMPER
+	li "ROCKET"        ; ROCKET
+	li "HIỀN GIẢ"      ; SAGE
+	li "ĐỒNG CỐT"      ; MEDIUM
+	li "TRƯỢT VÁN"     ; BOARDER
+	li "FAN #MON"      ; #FAN
+	li "KIMONO"        ; KIMONO GIRL
+	li "SONG SINH"     ; TWINS
+	li "FAN #MON"      ; #FAN
+	li "TRAINER"       ; <PKMN> TRAINER
+	li "THỦ LĨNH"      ; LEADER
+	li "CẢNH SÁT"      ; OFFICER
+	li "ROCKET"        ; ROCKET
+	li "BÍ ẨN"         ; MYSTICALMAN
 	assert_list_length NUM_TRAINER_CLASSES

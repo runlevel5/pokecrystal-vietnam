59f2aa12274200a70f52cd5e526610972dec2ecf
Trung Lê <8@tle.id.au>
2026-02-01 13:07:06 +1100

    Add 3-page Vietnamese input system for box naming
    - Replace BoxNameInputLower/Upper with BoxNameInput1/2/3
    - Update NamingScreen_ApplyTextInputMode to handle 3 box pages
    - Update .select logic to wrap at page 3 for boxes, page 5 for regular naming
    - Adjust screen layout: box naming now uses row 7 (5 rows) instead of row 6 (6 rows)
    - Fits within bank4 size limit (16KB)

--- data/text/name_input_chars.asm
+++ data/text/name_input_chars.asm
@@ -35,18 +35,23 @@ NameInputPage5:
 	db "                 "
 	db "tiếp   xoá  xong "
 
-BoxNameInputLower:
-	db "a b c d e f g h i"
-	db "j k l m n o p q r"
-	db "s t u v w x y z  "
-	db "é 'd 'l 'm 'r 's 't 'v 0"
-	db "1 2 3 4 5 6 7 8 9"
-	db "UPPER  DEL   END "
+BoxNameInput1:
+	db "a ă â b c d đ e ê"
+	db "g h i j k l m n o"
+	db "ô ơ p q r s t u ư"
+	db "v x y z          "
+	db "tiếp   xoá  xong "
 
-BoxNameInputUpper:
-	db "A B C D E F G H I"
-	db "J K L M N O P Q R"
-	db "S T U V W X Y Z  "
-	db "× ( ) :   [ ] <PK> <MN>"
-	db "- ? ! ♂ ♀ / . , &"
-	db "lower  DEL   END "
+BoxNameInput2:
+	db "á à ả ã ạ é è ẻ ẽ"
+	db "ẹ i í ì ỉ ĩ ị ó ò"
+	db "ỏ õ ọ ú ù ủ ũ ụ y"
+	db "ý ỳ ỷ ỹ ỵ ắ ằ ẳ ẵ"
+	db "tiếp   xoá  xong "
+
+BoxNameInput3:
+	db "ặ ấ ầ ẩ ẫ ậ"
+	db "1 2 3 4 5 6 7 8 9"
+	db "0 - ? ! . , / ♂ ♀"
+	db "× ( ) : & [ ] <PK> <MN>"
+	db "tiếp   xoá  xong "

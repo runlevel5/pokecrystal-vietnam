02da7aad64f61173399099a255f76a9e5e4b6991
Trung Lê <8@tle.id.au>
2026-02-01 15:20:49 +1100

    Translate 4 phone system files to Vietnamese (Erin, Gaven)
    - Translate Erin overworld file
    - Translate Gaven callee, caller, overworld files
    - All translations verified with character limit linter (≤18 chars/line)

--- data/phone/text/gaven_overworld.asm
+++ data/phone/text/gaven_overworld.asm
@@ -1,63 +1,61 @@
 GavenAskNumber1Text:
-	text "Wow, you're tough."
-	line "I haven't battled"
+	text "Wow, cậu mạnh ghê"
+	line "Mình lâu rồi mới"
 
-	para "that seriously in"
-	line "a long time."
+	para "chiến đấu nghiêm"
+	line "túc thế."
 
-	para "Could I get your"
-	line "phone number?"
+	para "Cho mình xin SĐT"
+	line "cậu được không?"
 
-	para "I'd like to battle"
-	line "again when I heal"
-
-	para "@"
+	para "Mình muốn đấu lại"
+	line "khi chữa @"
 	text_ram wStringBuffer4
-	text " and the"
-	line "rest of my team."
+
+	para "và các bạn khác"
+	line "trong đội."
 	done
 
 GavenAskNumber2Text:
-	text "Could I get your"
-	line "phone number?"
+	text "Cho mình xin SĐT"
+	line "cậu được không?"
 
-	para "I'd like to battle"
-	line "again when I heal"
-
-	para "@"
+	para "Mình muốn đấu lại"
+	line "khi chữa @"
 	text_ram wStringBuffer4
-	text " and the"
-	line "rest of my team."
+
+	para "và các bạn khác"
+	line "trong đội."
 	done
 
 GavenNumberAcceptedText:
-	text "How should I beat"
-	line "you? I'm tormented"
+	text "Làm sao để thắng"
+	line "cậu? Mình luôn bị"
 
-	para "by those thoughts"
-	line "all the time…"
+	para "ám ảnh bởi ý nghĩ"
+	line "đó…"
 	done
 
 GavenNumberDeclinedText:
-	text "OK… I understand…"
-	line "But if you change"
+	text "OK… Mình hiểu…"
+	line "Nhưng nếu đổi ý,"
 
-	para "your mind, give me"
-	line "a shout anytime."
+	para "hãy gọi cho mình"
+	line "bất cứ lúc nào."
 	done
 
 GavenPhoneFullText:
-	text "Your phone's"
-	line "memory is full."
+	text "Bộ nhớ điện thoại"
+	line "của cậu đầy rồi."
 
-	para "You can't register"
-	line "my number."
+	para "Không thể đăng ký"
+	line "SĐT của mình."
 	done
 
 GavenRematchText:
-	text "Hi! I've been"
-	line "waiting for you!"
+	text "Chào! Mình đợi cậu"
+	line "rồi đấy!"
 
-	para "This time, I'm"
-	line "going to win!"
+	para "Lần này, mình sẽ"
+	line "thắng cho xem!"
 	done

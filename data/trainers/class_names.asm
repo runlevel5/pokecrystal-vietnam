bd22c7265057b35f2787457607fdc347320c955e
Trung Lê <8@tle.id.au>
2026-02-03 10:43:10 +1100

    Update trainer class translations
    Changed 11 trainer class names to match user specifications:
    
    - LEADER: THỦ LĨNH → TH.LĨNH (abbreviated form, 16 occurrences)
    - SCIENTIST: NHÀ KHOA HỌC → HỌC GIẢ (scholar/scientist)
    - COOLTRAINER: CAO THỦ → TUYỂN THỦ (selected player/elite trainer)
    - #MANIAC: FAN #MON → CUỒNG # (crazy/obsessed with #)
    - SKIER: VĐV TRƯỢT → TRƯỢT TUYẾT (snow skiing)
    - BUG CATCHER: BẮT SÂU → BỌ THỦ (bug keeper/handler)
    - FISHER: CÂU CÁ → CẦN THỦ (fishing expert/master)
    - SWIMMER♂: BƠI LỘI♂ → VĐV BƠI♂ (swimming athlete male)
    - SWIMMER♀: BƠI LỘI♀ → VĐV BƠI♀ (swimming athlete female)
    - BURGLAR: ĐẠO CHÍCH → TRỘM (thief - simpler term)
    - #FAN: FAN #MON → #FAN (2 occurrences)
    
    Total changes: 11 unique trainer classes, 22 total lines modified

--- data/trainers/class_names.asm
+++ data/trainers/class_names.asm
@@ -1,14 +1,14 @@
 TrainerClassNames::
 ; entries correspond to trainer classes (see constants/trainer_constants.asm)
 	list_start TRAINER_CLASS_NAME_LENGTH - 1
-	li "THỦ LĨNH"      ; LEADER (Gym Leader)
-	li "THỦ LĨNH"      ; LEADER
-	li "THỦ LĨNH"      ; LEADER
-	li "THỦ LĨNH"      ; LEADER
-	li "THỦ LĨNH"      ; LEADER
-	li "THỦ LĨNH"      ; LEADER
-	li "THỦ LĨNH"      ; LEADER
-	li "THỦ LĨNH"      ; LEADER
+	li "TH.LĨNH"       ; LEADER (Gym Leader)
+	li "TH.LĨNH"       ; LEADER
+	li "TH.LĨNH"       ; LEADER
+	li "TH.LĨNH"       ; LEADER
+	li "TH.LĨNH"       ; LEADER
+	li "TH.LĨNH"       ; LEADER
+	li "TH.LĨNH"       ; LEADER
+	li "TH.LĨNH"       ; LEADER
 	li "ĐỐI THỦ"      ; RIVAL
 	li "GS.#MON"       ; #MON PROF. (Professor)
 	li "TỨ ĐẠI"        ; ELITE FOUR
@@ -17,37 +17,37 @@ TrainerClassNames::
 	li "TỨ ĐẠI"        ; ELITE FOUR
 	li "TỨ ĐẠI"        ; ELITE FOUR
 	li "VÔ ĐỊCH"       ; CHAMPION
-	li "THỦ LĨNH"      ; LEADER
-	li "THỦ LĨNH"      ; LEADER
-	li "THỦ LĨNH"      ; LEADER
-	li "NHÀ KHOA HỌC"  ; SCIENTIST
-	li "THỦ LĨNH"      ; LEADER
+	li "TH.LĨNH"       ; LEADER
+	li "TH.LĨNH"       ; LEADER
+	li "TH.LĨNH"       ; LEADER
+	li "HỌC GIẢ"       ; SCIENTIST
+	li "TH.LĨNH"       ; LEADER
 	li "THIẾU NIÊN"    ; YOUNGSTER
 	li "HỌC SINH"      ; SCHOOLBOY
 	li "CHIM THỦ"      ; BIRD KEEPER
 	li "THIẾU NỮ"      ; LASS
-	li "THỦ LĨNH"      ; LEADER
-	li "CAO THỦ"       ; COOLTRAINER (male)
-	li "CAO THỦ"       ; COOLTRAINER (female)
+	li "TH.LĨNH"       ; LEADER
+	li "TUYỂN THỦ"     ; COOLTRAINER (male)
+	li "TUYỂN THỦ"     ; COOLTRAINER (female)
 	li "MỸ NHÂN"       ; BEAUTY
-	li "FAN #MON"      ; #MANIAC
+	li "CUỒNG #"       ; #MANIAC
 	li "ROCKET"        ; ROCKET
 	li "QUÝ ÔNG"       ; GENTLEMAN
-	li "VĐV TRƯỢT"     ; SKIER
+	li "TRƯỢT TUYẾT"   ; SKIER
 	li "GIÁO VIÊN"     ; TEACHER
-	li "THỦ LĨNH"      ; LEADER
-	li "BẮT SÂU"       ; BUG CATCHER
-	li "CÂU CÁ"        ; FISHER
-	li "BƠI LỘI♂"      ; SWIMMER♂
-	li "BƠI LỘI♀"      ; SWIMMER♀
+	li "TH.LĨNH"       ; LEADER
+	li "BỌ THỦ"        ; BUG CATCHER
+	li "CẦN THỦ"       ; FISHER
+	li "VĐV BƠI♂"      ; SWIMMER♂
+	li "VĐV BƠI♀"      ; SWIMMER♀
 	li "THỦY THỦ"      ; SAILOR
 	li "MỌT SÁCH"      ; SUPER NERD
 	li "ĐỐI THỦ"      ; RIVAL
 	li "TAY ĐÀN"       ; GUITARIST
 	li "LEO NÚI"       ; HIKER
 	li "TAY ĐUA"       ; BIKER
-	li "THỦ LĨNH"      ; LEADER
-	li "ĐẠO CHÍCH"     ; BURGLAR
+	li "TH.LĨNH"       ; LEADER
+	li "TRỘM"          ; BURGLAR
 	li "PHUN LỬA"      ; FIREBREATHER
 	li "TẠP KỸ"        ; JUGGLER
 	li "VÕ SĨ"         ; BLACKBELT
@@ -59,12 +59,12 @@ TrainerClassNames::
 	li "HIỀN GIẢ"      ; SAGE
 	li "ĐỒNG CỐT"      ; MEDIUM
 	li "TRƯỢT VÁN"     ; BOARDER
-	li "FAN #MON"      ; #FAN
+	li "#FAN"          ; #FAN
 	li "KIMONO"        ; KIMONO GIRL
 	li "SONG SINH"     ; TWINS
-	li "FAN #MON"      ; #FAN
+	li "#FAN"          ; #FAN
 	li "HLV"           ; <PKMN> TRAINER
-	li "THỦ LĨNH"      ; LEADER
+	li "TH.LĨNH"       ; LEADER
 	li "CẢNH SÁT"      ; OFFICER
 	li "ROCKET"        ; ROCKET
 	li "BÍ ẨN"         ; MYSTICALMAN

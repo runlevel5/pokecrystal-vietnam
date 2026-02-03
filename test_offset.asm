a9556005dbbb1e54f618c1c287a3cc33d411dfc3
Trung Lê <8@tle.id.au>
2026-02-03 17:44:16 +1100

    Clean up

--- test_offset.asm
+++
@@ -1,9 +0,0 @@
-INCLUDE "constants/hardware_constants.asm"
-INCLUDE "constants/wram_constants.asm"
-
-SECTION "Test", ROM0
-test:
-    PRINTLN "wTilemap = {wTilemap}"
-    PRINTLN "wAttrmap = {wAttrmap}"
-    PRINTLN "wAttrmap - wTilemap = {d:wAttrmap - wTilemap}"
-    PRINTLN "Hex: {wAttrmap - wTilemap}"
